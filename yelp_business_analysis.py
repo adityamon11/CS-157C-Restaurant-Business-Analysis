2546385bc73966acd1253cc2c8d1292a28b1dd6e
Nicolas Guerrero <Guerreronicolas1872@gmail.com>
2022-05-06 19:56:07 -0700

    Add use cases 3, 12, and 15
    May need to clean up use case 3. Also addresses some bug fixes in various
    areas. Check the diff for all the modifications.

--- yelp_business_analysis.py
+++ yelp_business_analysis.py
@@ -3,6 +3,8 @@ import os
 import pymongo
 import json
 import io
+import re
+import datetime
 from pymongo import MongoClient
 from bson.objectid import ObjectId
 from bson.son import SON
@@ -22,9 +24,6 @@ def main():
     userCollection = db["userCollection"]
     tipCollection = db["tipCollection"]
 
-    # y = client.list_database_names
-    # print(y)
-
     while True:
         print("Welcome to the YELP Business Analysis Tool")
         print("Choose an option from the following: ")
@@ -36,7 +35,7 @@ def main():
         print("6. Find businesses with review counts greater than X and display hours")
         print("7. Find restaurants in location X where dogs are allowed and wifi is free")
         print("8. Find average star rating for every state. Display the state and the star ratings")
-        print("9. Find restaurants in a certain zip code where you don’t have to make reservations and restaurant attire is casual")
+        print("9. Find restaurants in a certain zip code where you don’t have to make reservations")
         print("10. Create users with given parameters")
         print("11. Create business with given parameters")
         print("12. Update user reviews")
@@ -79,7 +78,7 @@ def main():
             print("Selected 2")
             print("Use Case: List of users who provide the most number of compliments")
             results = db.tipCollection.aggregate([
-                {"$group": {_id:"$compliment_count",user_IDs: {"$push":"$user_id"}}}
+                {"$group": {"_id":"$compliment_count","user_IDs": {"$push":"$user_id"}}},
                 {"$sort":{"_id":-1}},
                 {"$limit":1}
                 ])
@@ -93,6 +92,50 @@ def main():
 
         elif user_val == '3':
             print("Selected 3")
+            print("Use Case: Find restaurants that are open after Y Time and have parking and valet service")
+            # Get a location hint to narrow down the queries desired
+            # loc_hint = input("What zipcode would you like to check? (Or press enter to skip) ")
+            # Day input
+            day = ''
+            day_regex = "Monday|Tuesday|Wednesday|Thursday|Friday|Saturday|Sunday"
+            while len(day) <= 0:
+                day = input("What day would you like to check? (Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday)")
+                if re.fullmatch(day_regex, day) == None:
+                    print("Day not recognized. Please try again.")
+                    day = ''
+            # Time input
+            time_str = ''
+            time_regex = "(1?[0-9])|(2[0-4])|([0-9](a|p)m)|(1[0-2](a|p)m)"
+            while len(time_str) <= 0:
+                time_str = input("After what time should we look for? (Military time 0 - 23 only)")
+                if re.fullmatch("1?[0-9]|2[0-3]", time_str) == None:
+                    print("Time not recognized. Please try again")
+                    time_str = ''
+            time_dt = int(time_str)
+            # Filter into query
+            query = {
+                "categories": "Restaurants",
+                "attributes.BusinessParking": {"$exists": True},
+                "attributes.BusinessParking.valet": True,
+                "hours": {"$ne": None}
+            }
+            res = businessCollection.find(query).limit(30)
+            # Since the hours are stored as string, queries need to happen first, then the range
+            # can be checked. It might be worthwhile to convert it to a tuple of start and end times.
+            valid = []
+            for doc in res:
+                if day in doc["hours"]:
+                    st, ed = doc["hours"][day].split("-")
+                    st = int(st.split(":")[0])
+                    ed = int(ed.split(":")[0])
+                    if time_dt in range(st,ed):
+                        valid.append(doc)
+            if len(valid) <= 0:
+                print("No results found")
+            else:
+                for doc in valid: print(doc)
+            proceed = input("Press any key to continue ")
+
         elif user_val == '4':
             print("Selected 4")
             print("Use Case: Find number of restaurants that are open/closed in a zipcode")
@@ -117,13 +160,13 @@ def main():
         elif user_val == '5':
             print("Selected 5")
             print("Use Case: Find restaurants in a certain zip code that provides delivery services.")
-            businessZip = input(“Enter postal code: ”)
+            businessZip = input("Enter postal code: ")
             
             restaurants=db.businessCollection.find({"postal_code":businessZip, "attributes.RestaurantsDelivery":"True"},{"business_id":1,"name":1}).limit(10)
             
             if len(list(results.clone())) > 0:
                 for res in restaurants:
-                print(res)
+                    print(res)
             else:
                 print("No records found!")
             
@@ -132,7 +175,7 @@ def main():
             continue
 
         elif user_val == '6':
-            print("Selected 6")
+            print("Selected 6") #TODO
         elif user_val == '7':
             print("Selected 7")
             print("Use Case: Find restaurants in location X where dogs are allowed and wifi is free")
@@ -166,7 +209,20 @@ def main():
 
 
         elif user_val == '9':
-            print("Selected 9")
+            try:
+                print("Selected 9")
+                print("Use case: Find restaurants in a certain zip code where you don’t have to make reservations")
+                z = input("Enter the postal code to check: ")
+                query = {
+                    "postal_code": z,
+                    "attributes.RestaurantsReservation": False
+                }
+                res = businessCollection.find(query)
+                for r in res:
+                    print(result)
+            except Exception as e:
+                print(e)
+            proceed = input("Press any key to continue ")
         elif user_val == '10':
             print("Selected 10")
             print("Use Case: Create user with given parameters")
@@ -260,7 +316,22 @@ def main():
             continue
 
         elif user_val == '15':
-            print("Selected 15")
+            try:
+                print("Selected 15")
+                print("Use Case: Find sum of users who joined yelp since date X")
+                dt_str = input("Enter a date (YYYY-MM-DD): ")
+                y, m, d = dt_str.split("-")
+                dt_obj = datetime.datetime(int(y), int(m), int(d))
+                pipeline = [
+                    {"$match": {"yelping_since": {"$gte": dt_obj}}},
+                    {"$count": "yelpers"}
+                ]
+                res = userCollection.aggregate(pipeline)
+                print(res)
+            except Exception as e:
+                print(e)
+            proceed = input("Press any key to continue ")
+
         elif user_val == '16':
             print("Selected 16")
             print("Use Case: Find top 10 businesses in location X with highest number of check in times")
